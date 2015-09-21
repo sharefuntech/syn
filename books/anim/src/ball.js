3d6ae609816d45b3422ba72123a0307589388e2a
guoweish <guoweish@163.com>
2015-09-21 16:36:35 +0800

    0921office

---
+++ books/anim/src/ball.js
@@ -0,0 +1,28 @@
+function Ball(radius, color) {
+	if(radius == undefined) { radius = 30}
+	if(color == undefined) { color = 'teal'}
+	this.x = 0;
+	this.y = 0;
+	this.radius =radius;
+	this.rotation = 0;
+	this.scaleX = 1;
+	this.scaleY = 1;
+	this.color = utils.parseColor(color);
+	this.lineWidth = 1;
+}
+
+Ball.prototype.draw = function(context) {
+	context.save();
+	context.translate(this.x, this.y);
+	context.rotate(this.rotation);
+	context.scale(this.scaleX, this.scaleY);
+	context.lineWidth = this.lineWidth;
+	context.fillStyle = this.color;
+
+	context.beginPath();
+	context.arc(0, 0, this.radius, 0, (Math.PI*2), true);
+	context.closePath();
+	context.fill();
+	// context.stroke();
+	context.restore();
+};
