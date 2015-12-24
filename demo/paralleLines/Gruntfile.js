749e198a8327a44534ef7f97a80b1bf446a6e076
guoweish <guoweish@163.com>
2015-12-24 13:40:08 +0800

    1224o1

---
+++ demo/paralleLines/Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function (grunt) {
+  // 项目配置
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      'myTarget': {
+        "files": {
+          'dest/demo.min.js': ['src/demo.js']
+        }
+      }
+    },
+    cssmin: {
+      compress: {
+        files: {
+          'dest/style.min.css': ['src/style.css']
+        }
+      }
+    }
+  });
+  // 加载提供"uglify"任务的插件
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  // 默认任务
+  grunt.registerTask('default', ['uglify', 'cssmin']);
+  // grunt.registerTask(['uglify', 'cssmin']);
+}
