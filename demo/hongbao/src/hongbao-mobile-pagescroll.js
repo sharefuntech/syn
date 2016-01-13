0919f566d6af6c5bf7e29d6c52e8b80875261b19
guoweish <guoweish@163.com>
2016-01-13 23:02:02 +0800

    0113h

--- demo/hongbao/src/hongbao-mobile-pagescroll.js
+++ demo/hongbao/src/hongbao-mobile-pagescroll.js
@@ -5,171 +5,179 @@ $(".main").onepage_scroll({loop: false});
 
 //###############################################################
 //### viz start ###############################################
+// 视口查询
+// var viewportWidth = window.innerWidth; // ios safari刷新后svg图像莫名变大
+// var viewportHeight = window.innerHeight;
+var viewportWidth = document.documentElement.clientWidth;
+var viewportHeight = document.documentElement.clientHeight;
+// console.log(viewportWidth);
+// console.log(viewportHeight);
+
 d3.csv('data/hongbao.csv', function(data) {
     data.forEach(function(d) {
         d.money_received = +d.money_received;
         // d.hb_id = +d.hb_id;
     });
     // console.log(data);
-
-    var nodes = [];
-    var links = [];
-
-    // d3.range(66).map(function(d, i) {
-    //     var node = {name: i+1};
-    //     nodes.push(node);
-    // });
-
-    data.forEach(function(d) {
-        var link = {
-            source: d.people_name,
-            target: d.hb_id
-        };
-        links.push(link);
-    });
-    // console.log(links);
-
-    // console.log(nodes);
-
-    var dataNestedById = d3.nest()
-        .key(function(d) {
-            return d.hb_id;
-        })
-        .entries(data);
-    // console.log(dataNestedById);
-    // console.log(data);
-    dataNestedById.forEach(function(d) {
-        // console.log(d.key);
-        var node = {name: d.key};
-        nodes.push(node);
-    });
-
-    var dataNestedByPeople = d3.nest()
-        .key(function(d) {
-            return d.people_name;
-        })
-        .entries(data);
-    // console.log(dataNestedByPeople);
-    dataNestedByPeople.forEach(function(d) {
-        // console.log(d.key);
-        var node = {name: d.key};
-        nodes.push(node);
-    });
-    // console.log(nodes);
-
-    var forceData = {
-        nodes: nodes,
-        links: links
-    };
-    // console.log(forceData);
-    var width = 300;
-    var height = 400;
-
-    var svg_1 = d3.select("#vizContainer-1")
-        .append("svg")
-        .attr("width",width)
-        .attr("height",height - 50);
-
-    var forceDataIndexed = addIndex(forceData);
-
-    drawForce(forceDataIndexed, svg_1);
-    drawBundle(data, '#vizContainer-2');
-
-    function addIndex(data) {
-        var dataIndexed = {};
-        var nodesArray = [];
-        var nodesData = data.nodes;
-        var linksData = data.links;
-        var linkIndexed;
-
-        for(keys in nodesData) {
-            nodesArray.push(nodesData[keys].name);
-        }
-
-        linkIndexed = linksData.map(function(link) {
-            var sourceData = link.source;
-            var targetData = link.target;
-
-            var sourceIndex = nodesArray.indexOf(sourceData);
-            var targetIndex = nodesArray.indexOf(targetData);
-
-            return {source: sourceIndex, target: targetIndex};
-        });
-
-        dataIndexed.nodes = nodesData;
-        dataIndexed.links = linkIndexed;
-
-        return dataIndexed;
-    }
-
-    function drawForce(data, svg) {
-        var nodes = data.nodes;
-        var edges = data.links;
-
-        var force = d3.layout.force()
-            .nodes(nodes)   //指定节点数组
-            .links(edges)   //指定连线数组
-            .size([width-30,height-30]) //指定范围
-            .linkDistance(100)  //指定连线长度
-            .charge([-300]);  //相互之间的作用力
-
-        force.start();  //开始作用
-
-        var color = d3.scale.category20();
-
-        //添加连线
-        var svg_edges = svg.selectAll("line")
-            .data(edges)
-            .enter()
-            .append("line")
-            .style("stroke","#eee")
-            .style("stroke-width",1);
-
-        //添加节点
-        var svg_nodes = svg.selectAll("circle")
-            .data(nodes)
-            .enter()
-            .append("circle")
-            .attr("r",10)
-            .style('fill', 'gray')
-            .style('fill-opacity', .7)
-            .call(force.drag);  //使得节点能够拖动
-
-        //添加描述节点的文字
-        var svg_texts = svg.selectAll("text")
-            .data(nodes)
-            .enter()
-            .append("text")
-            .style("fill", "black")
-            .style('font-size', 10)
-            .attr('text-anchor', 'middle')
-            .attr("dx", 0)
-            .attr("dy", -8)
-            .text(function(d){
-                return d.name;
-            });
-
-
-        force.on("tick", function(){  //对于每一个时间间隔
-            //更新连线坐标
-            svg_edges.attr("x1",function(d){ return d.source.x; })
-                .attr("y1",function(d){ return d.source.y; })
-                .attr("x2",function(d){ return d.target.x; })
-                .attr("y2",function(d){ return d.target.y; });
-
-            //更新节点坐标
-            svg_nodes.attr("cx",function(d){ return d.x; })
-                .attr("cy",function(d){ return d.y; });
-
-            //更新文字坐标
-            svg_texts.attr("x", function(d){ return d.x; })
-                .attr("y", function(d){ return d.y; });
-        });
-    }
+    // 在这里可以做不同设备的视口设置，代替css方案
+    var svgMinCanvas = d3.min([viewportWidth, viewportHeight]);
+    // console.log(svgMaxCanvas);
+    drawForce(data, '#vizContainer-1', svgMinCanvas, svgMinCanvas)
+    drawBundle(data, '#vizContainer-2', svgMinCanvas, svgMinCanvas);
 
     //###################################################################
     //### bundle start #################################################
-    function drawBundle(data, container) {
+    function drawForce(data, container, svgWidth, svgHeight) {
+        var nodes = [];
+        var links = [];
+
+        data.forEach(function(d) {
+            var link = {
+                source: d.people_name,
+                target: d.hb_id
+            };
+            links.push(link);
+        });
+
+        var dataNestedById = d3.nest()
+            .key(function(d) {
+                return d.hb_id;
+            })
+            .entries(data);
+
+        dataNestedById.forEach(function(d) {
+            // console.log(d.key);
+            var node = {name: d.key};
+            nodes.push(node);
+        });
+
+        var dataNestedByPeople = d3.nest()
+            .key(function(d) {
+                return d.people_name;
+            })
+            .entries(data);
+        // console.log(dataNestedByPeople);
+        dataNestedByPeople.forEach(function(d) {
+            // console.log(d.key);
+            var node = {name: d.key};
+            nodes.push(node);
+        });
+        // console.log(nodes);
+
+        var forceData = {
+            nodes: nodes,
+            links: links
+        };
+        // console.log(forceData);
+        var width = svgWidth;
+        var height = svgHeight;
+
+        var svg = d3.select(container)
+            .append("svg")
+            .attr("width",width)
+            .attr("height",height - 50);
+
+        var forceDataIndexed = addIndex(forceData);
+
+        setForce(forceDataIndexed, svg);
+
+        function addIndex(data) {
+            var dataIndexed = {};
+            var nodesArray = [];
+            var nodesData = data.nodes;
+            var linksData = data.links;
+            var linkIndexed;
+
+            for(keys in nodesData) {
+                nodesArray.push(nodesData[keys].name);
+            }
+
+            linkIndexed = linksData.map(function(link) {
+                var sourceData = link.source;
+                var targetData = link.target;
+
+                var sourceIndex = nodesArray.indexOf(sourceData);
+                var targetIndex = nodesArray.indexOf(targetData);
+
+                return {source: sourceIndex, target: targetIndex};
+            });
+
+            dataIndexed.nodes = nodesData;
+            dataIndexed.links = linkIndexed;
+
+            return dataIndexed;
+        }
+
+        function setForce(data, svg) {
+            var nodes = data.nodes;
+            var edges = data.links;
+
+            var force = d3.layout.force()
+                .nodes(nodes)   //指定节点数组
+                .links(edges)   //指定连线数组
+                .size([width-30,height-30]) //指定范围
+                .linkDistance(100)  //指定连线长度
+                .charge([-300]);  //相互之间的作用力
+
+            force.start();  //开始作用
+
+            var color = d3.scale.category20();
+
+            //添加连线
+            var svg_edges = svg.selectAll("line")
+                .data(edges)
+                .enter()
+                .append("line")
+                .style("stroke","#eee")
+                .style("stroke-width",1);
+
+            //添加节点
+            var svg_nodes = svg.selectAll("circle")
+                .data(nodes)
+                .enter()
+                .append("circle")
+                .attr("r",10)
+                .style('fill', 'gray')
+                .style('fill-opacity', .7)
+                .call(force.drag);  //使得节点能够拖动
+
+            //添加描述节点的文字
+            var svg_texts = svg.selectAll("text")
+                .data(nodes)
+                .enter()
+                .append("text")
+                .style("fill", "black")
+                .style('font-size', 10)
+                .attr('text-anchor', 'middle')
+                .attr("dx", 0)
+                .attr("dy", -8)
+                .text(function(d){
+                    return d.name;
+                });
+
+            force.on("tick", function(){  //对于每一个时间间隔
+                //更新连线坐标
+                svg_edges.attr("x1",function(d){ return d.source.x; })
+                    .attr("y1",function(d){ return d.source.y; })
+                    .attr("x2",function(d){ return d.target.x; })
+                    .attr("y2",function(d){ return d.target.y; });
+
+                //更新节点坐标
+                svg_nodes.attr("cx",function(d){ return d.x; })
+                    .attr("cy",function(d){ return d.y; });
+
+                //更新文字坐标
+                svg_texts.attr("x", function(d){ return d.x; })
+                    .attr("y", function(d){ return d.y; });
+            });
+        }
+
+    }
+    //### bundle end #################################################
+
+    //###################################################################
+    //### bundle start #################################################
+    function drawBundle(data, container, svgWidth, svgHeight) {
         var bundleLinks = [];
 
         data.forEach(function(d) {
@@ -209,8 +217,8 @@ d3.csv('data/hongbao.csv', function(data) {
             children: nodesAll
         };
 
-        var width  = 300;	//SVG绘制区域的宽度
-        var height = 300;	//SVG绘制区域的高度
+        var width  = svgWidth;	//SVG绘制区域的宽度
+        var height = svgHeight;	//SVG绘制区域的高度
 
         var svg = d3.select(container)			//选择<body>
                     .append("svg")
